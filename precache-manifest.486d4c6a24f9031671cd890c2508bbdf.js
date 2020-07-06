47a6f2bfd64f243d203c0fa75f0459b7871d4411
Vitali Makhatadze <vitomaxatadze@gmail.com>
2020-07-06 15:38:34 +0400

    Updates

---
+++ precache-manifest.486d4c6a24f9031671cd890c2508bbdf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4246c1e43124da368cef496b13ee6a",
+    "url": "/student_card/index.html"
+  },
+  {
+    "revision": "a5438daddfa8840dd404",
+    "url": "/student_card/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1c59c95ea4e47cbd57f0",
+    "url": "/student_card/static/css/main.9bee0681.chunk.css"
+  },
+  {
+    "revision": "a5438daddfa8840dd404",
+    "url": "/student_card/static/js/2.348305cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/student_card/static/js/2.348305cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c59c95ea4e47cbd57f0",
+    "url": "/student_card/static/js/main.5d68b8aa.chunk.js"
+  },
+  {
+    "revision": "61dbaa940e07bda34fe2",
+    "url": "/student_card/static/js/runtime-main.f65553b3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/student_card/static/media/SEGA.12a3db8b.woff"
+  }
+]);
